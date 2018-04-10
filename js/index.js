b236d1ac70366e82298d642449ac1f3ea2a728f2
hiroki yamada <medaka3-lj@infoseek.jp>
2018-04-10 10:11:32 +0900

    2018/04/10

--- js/index.js
+++ js/index.js
@@ -26,6 +26,18 @@ var Index = {
 		    .empty()
 		    .append(out_html.filter('#content')[0].innerHTML);
 	    });
+	});
+	$('.article-items').click(function(data) {
+	    var filename = $(this).data("article-filename");
+	    $.ajax(filename, {
+		timeout : 1000, // 1000 ms
+		datatype:'html'
+	    }).then(function(data){
+		var out_html = $($.parseHTML(data));
+		$('#main-article')
+		    .empty()
+		    .append(out_html.filter('#content')[0].innerHTML);
+	    });
 	    
 	});
     }
