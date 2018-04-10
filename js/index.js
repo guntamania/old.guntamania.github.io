cdb561bfcd415295a00110f1ef6fc56b0d4c79f2
hiroki yamada <medaka3-lj@infoseek.jp>
2018-04-10 10:16:40 +0900

    indexを変更

--- js/index.js
+++ js/index.js
@@ -2,9 +2,21 @@
 var Index = {
     onload: function() {
 	console.log("onload");
-	$('#aboutme').click(function() {
+	$('#menu-aboutme').click(function() {
 	    console.log("onclick");
 	    $.ajax('/about/about.html', {
+		timeout : 1000, // 1000 ms
+		datatype:'html'
+	    }).then(function(data){
+		var out_html = $($.parseHTML(data));
+		$('#main-article')
+		    .empty()
+		    .append(out_html.filter('#content')[0].innerHTML);
+	    });
+	});
+	$('#menu-articles').click(function() {
+	    console.log("onclick");
+	    $.ajax('/articles/index.html', {
 		timeout : 1000, // 1000 ms
 		datatype:'html'
 	    }).then(function(data){
