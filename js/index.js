07811524b4df727208f00a83344027eeaecef10e
hiroki yamada <medaka3-lj@infoseek.jp>
2018-04-15 12:33:06 +0900

    2018-04-15

--- js/index.js
+++ js/index.js
@@ -39,19 +39,6 @@ var Index = {
 		    .append(out_html.filter('#content')[0].innerHTML);
 	    });
 	});
-	$('.article-items').click(function(data) {
-	    var filename = $(this).data("article-filename");
-	    $.ajax(filename, {
-		timeout : 1000, // 1000 ms
-		datatype:'html'
-	    }).then(function(data){
-		var out_html = $($.parseHTML(data));
-		$('#main-article')
-		    .empty()
-		    .append(out_html.filter('#content')[0].innerHTML);
-	    });
-	    
-	});
     }
 };
 
@@ -60,3 +47,19 @@ $(document).ready(function() {
     console.log("ready");
     Index.onload();
 });
+
+$('.article-items').ready(function() {
+    $('.article-items').click(function(data) {
+	var filename = $(this).data("article-filename");
+	$.ajax(filename, {
+	    timeout : 1000, // 1000 ms
+	    datatype:'html'
+	}).then(function(data){
+	    var out_html = $($.parseHTML(data));
+	    $('#main-article')
+		.empty()
+		.append(out_html.filter('#content')[0].innerHTML);
+	});
+	
+    });
+}
